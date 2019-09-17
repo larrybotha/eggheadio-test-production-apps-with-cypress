c964c3bbad34b62f4c17caa5c7efd10cfb887d1d
Larry Botha <larry@fixate.it>
2019-09-17 00:17:41 -0500

    add examples and annotations of custom commands and logs

---
+++ cypress/integration/09-todos.spec.js
@@ -0,0 +1,32 @@
+import {arrayOf} from 'test-data-bot';
+import {todoItemsBuilder} from '../fixtures/generators/todo-item';
+
+describe('Todo application', () => {
+  it('loads the page', async () => {
+    const numTodos = 3;
+    const todoItems = todoItemsBuilder(numTodos);
+
+    cy.server();
+    cy.route('/api/todos', todoItems).as('get-todos');
+
+    cy.visit('/');
+    cy.wait('@get-todos');
+
+    /**
+     * This sequence of commands is cumberson, so what we can benefit from
+     * moving this to a custom command in Cypress
+     */
+    cy.window()
+      .then($window => console.log($window.store))
+      .its('store')
+      .invoke('getState')
+      .should('deep.equal', {todos: todoItems, visibilityFilter: 'show_all'});
+
+    /**
+     * Use our custom command to retrieve the store
+     */
+    cy.getStoreState('todos').should('deep.equal', todoItems);
+
+    cy.findAllByTestId(/^todo-item/).should('have.length', numTodos);
+  });
+});
