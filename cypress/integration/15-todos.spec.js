4609494c8f0de09455ab16b514f865e7b09592ac
Larry Botha <larry@fixate.it>
2019-10-05 17:22:16 -0500

    seed database

--- cypress/integration/15-todos.spec.js
+++ cypress/integration/15-todos.spec.js
@@ -1,5 +1,17 @@
+const {todoItemsBuilder} = require('../fixtures/generators/todo-item');
+
 describe('Todo application with forced 404s', () => {
   it('seeds the database', function() {
-    cy.task('db:seed', {todos: [], users: []});
+    const todos = todoItemsBuilder();
+
+    /**
+     * run the seed task defined in cypress/plugins/index.js
+     */
+    cy.task('db:seed', {todos});
+
+    /**
+     * visit our app so we can see the data in our app
+     */
+    cy.visit('/');
   });
 });
